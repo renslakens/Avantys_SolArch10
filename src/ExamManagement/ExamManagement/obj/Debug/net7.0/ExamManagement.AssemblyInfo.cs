44df23a502a52b7c2b474c54d61ea3e18dc7a050
Ayrianna Chatlein <aechatlein@gmail.com>
2024-06-12 14:20:51 +0200

    chore: Update assembly version

--- src/ExamManagement/ExamManagement/obj/Debug/net7.0/ExamManagement.AssemblyInfo.cs
+++ src/ExamManagement/ExamManagement/obj/Debug/net7.0/ExamManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExamManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d34e75377b67cbd63330c0ceacd1e4f650f4a05")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExamManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExamManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
