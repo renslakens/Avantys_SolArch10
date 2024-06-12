3208b6131b379878528c06d54efb803831bc66a7
Ayrianna Chatlein <aechatlein@gmail.com>
2024-06-12 14:20:19 +0200

    Created models

---
+++ src/ExamManagement/ExamManagement/Models/Exam.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ExamManagement.Models
+{
+    public class Exam
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime StartDateTime { get; set; }
+
+        public DateTime EndDateTime { get; set; }
+
+        public double Grade { get; set; }
+
+        public int Credits { get; set; }
+
+        //TODO: change to class model
+        public string Class { get; set; }
+
+        //TODO: change to proctor model
+        public string Proctor { get; set; }
+
+        //TODO: change to module model
+        public string Module { get; set; }
+
+        public bool IsReviewed { get; set; }
+
+        public string Classroom { get; set; } 
+    }
+}
---
+++ src/ExamManagement/ExamManagement/Models/Proctor.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ExamManagement.Models
+{
+    public class Proctor
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
