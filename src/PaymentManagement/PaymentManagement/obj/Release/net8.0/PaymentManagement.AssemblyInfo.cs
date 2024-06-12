fb4206a0ba7daa8d65ecba105b51786b9970c5da
Niekelodius <niekelodius@gmail.com>
2024-06-12 13:41:06 +0200

    run all in docker

--- src/PaymentManagement/PaymentManagement/obj/Release/net8.0/PaymentManagement.AssemblyInfo.cs
+++ src/PaymentManagement/PaymentManagement/obj/Release/net8.0/PaymentManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20203b0d39ec925a8eab8c7deaf328eeaf4ab255")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b34e874b86bd2d5ebe997785d6ef25eadb98732d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
