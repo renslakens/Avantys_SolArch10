e74d96d1fd41081b652fc48e493c8aa7147520c6
Ayrianna Chatlein <aechatlein@gmail.com>
2024-06-26 09:30:26 +0200

    Added MongoID to events

--- src/ExamEventHandler/ExamEventHandler/Events/ExamConducted.cs
+++ src/ExamEventHandler/ExamEventHandler/Events/ExamConducted.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ExamManagement.Messaging;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace ExamManagement.Events
 {
     public class ExamConducted : Event
     {
+        [BsonIgnoreIfDefault]
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string MongoId { get; set; }
         public readonly string examId;
         public readonly string studentId;
         public readonly DateTime conductedDate;
